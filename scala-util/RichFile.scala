1d79e4668d5f28db4cbdbdf5b68dad86c5e54fed
Thomas Grottker <Thomas@.(none)>
2012-06-28 20:25:34 +0200

    added comments

--- scala-util/RichFile.scala
+++ scala-util/RichFile.scala
@@ -2,13 +2,26 @@ import java.io._
 import java.nio._
 import scala.io._
 
+/**
+  * Provides easy acces to a File.
+  */
 class RichFile(file: File) {
 	
+	// TODO add append
+	
+	/**
+	  * Returns the content of a file as string.
+	  */
 	def text = Source.fromFile(file).mkString
 	
-	def text_=(s: String) {
+	/**
+	  * Sets the context of the file to the given string.
+	  *
+	  * @param newContent The new content of the file.
+	  */
+	def text_=(newContent: String) {
 		val out = new PrintWriter(file)
-		try { out.print(s) }
+		try { out.print(newContent) }
 		finally { out.close }
 	}
 	
@@ -16,6 +29,9 @@ class RichFile(file: File) {
 
 object RichFile {
 	
+	/**
+	  * Provides an implicit conversion for java.io.File to RichFile.
+	  */
 	implicit def enrich(file: File) = new RichFile(file)
 	
 }
